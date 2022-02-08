c576012aa12b1e77271d9c49f41a584c58657bba
MKhodagholi <MKhodaagholi@gmail.com>
2022-02-08 14:26:19 +0330

    5. Intro to Styled Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+//Global Styled
+import GlobalStyle from "./components/GlobalStyle";
 
 import AboutUs from "./pages/AboutUs";
 
 function App() {
   return (
     <div>
+      <GlobalStyle />
       <AboutUs />
     </div>
   );
