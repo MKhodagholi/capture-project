189f158417f7518aa12b840d05ab3054d41a2299
MKhodagholi <MKhodaagholi@gmail.com>
2022-02-08 15:50:17 +0330

    8. add Nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 //Global Styled
 import GlobalStyle from "./components/GlobalStyle";
-
 import AboutUs from "./pages/AboutUs";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <div>
       <GlobalStyle />
+      <Nav />
       <AboutUs />
     </div>
   );
