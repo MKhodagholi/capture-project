d8e56da1a6328cf899af4ec2098803034056e099
MKhodagholi <MKhodaagholi@gmail.com>
2022-02-08 11:56:01 +0330

    3. Servises Section

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 import AboutSection from "../components/AboutSection";
+import ServisesSection from "../components/ServisesSection";
 
 const AboutUs = () => {
   return (
     <>
       <AboutSection />
+      <ServisesSection /> 
     </>
   );
 };
