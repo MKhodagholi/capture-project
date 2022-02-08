2bb4d580a53f000fcbd924b6ba9dd7b685ec955c
MKhodagholi <MKhodaagholi@gmail.com>
2022-02-08 22:20:57 +0330

    9. Intro to react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,28 @@ import React from "react";
 import GlobalStyle from "./components/GlobalStyle";
 import AboutUs from "./pages/AboutUs";
 import Nav from "./components/Nav";
+import OurWork from "./pages/OurWork";
+import ContactUs from "./pages/ContactUs";
+
+//Router
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div>
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Switch>
+        <Route exact path="/">
+          <AboutUs />
+        </Route>
+        <Route path="/work">
+          <OurWork />
+        </Route>
+        <Route>
+          <ContactUs path="/contact" />
+        </Route>
+      </Switch>
     </div>
   );
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import styled from "styled-components";
 
@@ -6,19 +7,19 @@ const Nav = () => {
   return (
     <StyledNav>
       <h1>
-        <a href="#" id="logo">
+        <Link to="/" id="logo">
           Capture
-        </a>
+        </Link>
       </h1>
       <ul>
         <li>
-          <a href="">1. About Us</a>
+          <Link to="/">1. About Us</Link>
         </li>
         <li>
-          <a href="">2. Our Work</a>
+          <Link to="/work">2. Our Work</Link>
         </li>
         <li>
-          <a href="">3. Contact Us</a>
+          <Link to="/contact">3. Contact Us</Link>
         </li>
       </ul>
     </StyledNav>
