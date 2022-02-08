4520aa2d3c5c87d91249e89be807f18a30e1f396
MKhodagholi <MKhodaagholi@gmail.com>
2022-02-08 23:33:11 +0330

    11. MovieDetail

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import AboutUs from "./pages/AboutUs";
 import Nav from "./components/Nav";
 import OurWork from "./pages/OurWork";
 import ContactUs from "./pages/ContactUs";
+import MovieDetail from "./pages/MovieDetail";
 
 //Router
 import { Route, Switch } from "react-router-dom";
@@ -18,8 +19,11 @@ function App() {
         <Route exact path="/">
           <AboutUs />
         </Route>
-        <Route path="/work">
+        <Route path="/work" exact>
           <OurWork />
+        </Route>
+        <Route path="/work/:id">
+          <MovieDetail />
         </Route>
         <Route>
           <ContactUs path="/contact" />
