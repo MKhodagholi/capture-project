40ad53e1f4337b4b217f0ecae99e50ba0e93d40d
MKhodagholi <MKhodaagholi@gmail.com>
2022-02-09 10:00:40 +0330

    13. Intro to page Animation

--- src/App.jsx
+++ src/App.jsx
@@ -8,27 +8,33 @@ import ContactUs from "./pages/ContactUs";
 import MovieDetail from "./pages/MovieDetail";
 
 //Router
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, useLocation } from "react-router-dom";
+//Animation
+import { AnimatePresence } from "framer-motion";
 
 function App() {
+  const location = useLocation();
+
   return (
     <div>
       <GlobalStyle />
       <Nav />
-      <Switch>
-        <Route exact path="/">
-          <AboutUs />
-        </Route>
-        <Route path="/work" exact>
-          <OurWork />
-        </Route>
-        <Route path="/work/:id">
-          <MovieDetail />
-        </Route>
-        <Route>
-          <ContactUs path="/contact" />
-        </Route>
-      </Switch>
+      <AnimatePresence exitBeforeEnter>
+        <Switch location={location} key={location.pathname}>
+          <Route exact path="/">
+            <AboutUs />
+          </Route>
+          <Route path="/work" exact>
+            <OurWork />
+          </Route>
+          <Route path="/work/:id">
+            <MovieDetail />
+          </Route>
+          <Route>
+            <ContactUs path="/contact" />
+          </Route>
+        </Switch>
+      </AnimatePresence>
     </div>
   );
 }
--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -3,14 +3,22 @@ import React from "react";
 import AboutSection from "../components/AboutSection";
 import ServisesSection from "../components/ServisesSection";
 import FaqSection from "../components/FaqSection";
+// Animations
+import { motion } from "framer-motion";
+import { pageAnimation } from "../animation";
 
 const AboutUs = () => {
   return (
-    <>
+    <motion.div
+      variants={pageAnimation}
+      initial="hidden"
+      animate="show"
+      exit="exit"
+    >
       <AboutSection />
       <ServisesSection />
       <FaqSection />
-    </>
+    </motion.div>
   );
 };
 
