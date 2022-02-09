abf19f14ec296af65778b5aab2cf5ef241885b1a
MKhodagholi <MKhodaagholi@gmail.com>
2022-02-09 16:53:46 +0330

    14. About Animaton

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -5,6 +5,7 @@ import home1 from "../img/home1.png";
 import { About, Description, Hide, Image } from "../styles";
 //Frame Motion
 import { motion } from "framer-motion";
+import { titleAnim, fade, photoAnim } from "../animation";
 
 const AboutSection = () => {
   // const titleAnim = {
@@ -28,25 +29,25 @@ const AboutSection = () => {
       <Description>
         <motion.div>
           <Hide>
-            <motion.h2>We work to make</motion.h2>
+            <motion.h2 variants={titleAnim}>We work to make</motion.h2>
           </Hide>
           <Hide>
-            <motion.h2>
+            <motion.h2 variants={titleAnim}>
               your <span>dreams</span> come
             </motion.h2>
           </Hide>
           <Hide>
-            <motion.h2>true.</motion.h2>
+            <motion.h2 variants={titleAnim}>true.</motion.h2>
           </Hide>
         </motion.div>
-        <p>
+        <motion.p variants={fade}>
           Contact us for any photography or videography ideas that you havve. We
           have professional with amazing skills
-        </p>
-        <button>Contact Us</button>
+        </motion.p>
+        <motion.button variants={fade}>Contact Us</motion.button>
       </Description>
       <Image>
-        <img src={home1} alt="guy with camera" />
+        <motion.img variants={photoAnim} src={home1} alt="guy with camera" />
       </Image>
     </About>
   );
