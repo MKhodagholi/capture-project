7453431a0821cf6c3823d43d4382299ea47940fd
MKhodagholi <MKhodaagholi@gmail.com>
2022-02-10 10:19:41 +0330

    19. useScroll and animation for faq and services componnet

--- src/components/ServisesSection.jsx
+++ src/components/ServisesSection.jsx
@@ -9,10 +9,18 @@ import home2 from "../img/home2.png";
 //Styles
 import { About, Description, Image } from "../styles";
 import styled from "styled-components";
+import { scrollReveal } from "../animation";
+import { useScroll } from "./useScroll";
 
 const ServisesSection = () => {
+  const [element, controls] = useScroll();
   return (
-    <Servises>
+    <Servises
+      variants={scrollReveal}
+      initial="hidden"
+      animate={controls}
+      ref={element}
+    >
       <Description>
         <h2>
           High <span>quality</span> servises
